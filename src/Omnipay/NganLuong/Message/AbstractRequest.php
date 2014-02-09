34d4d2948a56ba204dc1220353e0a29bf355d3c4
gianglv <levgiang@yahoo.com>
2014-02-09 22:14:08 +0700

    Use SoapClient

--- src/Omnipay/NganLuong/Message/AbstractRequest.php
+++ src/Omnipay/NganLuong/Message/AbstractRequest.php
@@ -3,6 +3,7 @@
 namespace Omnipay\NganLuong\Message;
 
 use SoapClient;
+use DOMDocument;
 /**
  * NganLuong Abstract Request
  */
@@ -108,13 +109,10 @@ abstract class AbstractRequest extends \Omnipay\Common\Message\AbstractRequest
     }
 
     public function sendData($data)
-    {
-        $url = $this->getEndpoint().'?'.http_build_query($data);
-        $httpResponse = $this->httpClient->get($url)->send();
-        
-        $soapClient = new SoapClient($this->getEndPoint());
-		$soapClient->__soapCall($this->getOperation(), $params);
-        return $this->createResponse($httpResponse->getBody());
+    {        
+        $soap_client = new SoapClient($this->getEndpoint(), array('encoding' => 'ISO-8859-1'));
+        $soap_response = $soap_client->__soapCall($this->getOperation(), $this->buildSoapParams($data));
+        return $this->createResponse($soap_response);
     }
 
     protected function getEndpoint()
@@ -125,5 +123,88 @@ abstract class AbstractRequest extends \Omnipay\Common\Message\AbstractRequest
     protected function createResponse($data)
     {
         return $this->response = new Response($this, $data);
+    }
+    
+    protected function buildSoapParams($data)
+    {
+    	$params = array(
+    			'merchant_site_code'	=> $this->merchantSiteCode,
+    			'checksum'				=> $this->_makeChecksum($data),
+    			'params'				=> '<params>'.$this->_convertArrayToXML($data).'</params>'
+    	);
+    	return $params;
+    }
+    private function _makeChecksum($params)
+    {
+    	$md5 = '';
+    	$keys = $this->_getMapKeys();
+    	foreach ($keys as $key) {
+    		$md5.= strval(@$params[$key]);
+    	}
+    	$md5.= $this->merchantPassword;
+    	return md5($md5);
+    }
+    
+    private function _convertXMLToArray($xml, $tag = 'params')
+    {
+    	$doc = new DOMDocument();
+    	$doc->loadXML($xml);
+    	$params = $doc->getElementsByTagName($tag);
+    	return $this->_getNodes($params->item(0));
+    }
+    
+    private function _getNodes($parent_node)
+    {
+    	$result = array();
+    	$nodes = $parent_node->childNodes;
+    	for ($i = 0; $i < $nodes->length; $i++) {
+    		$node = $nodes->item($i);
+    		if ($node->nodeType == 1) {
+    			if ($parent_node->getElementsByTagName($node->nodeName)->length == 1) {
+    				if ($this->_hasChildNodes($node)) {
+    					$result[$node->nodeName] = $this->_getNodes($node);
+    				} else {
+    					$result[$node->nodeName] = trim(html_entity_decode($node->nodeValue));
+    				}
+    			} else {
+    				if ($this->_hasChildNodes($node)) {
+    					$result[] = $this->_getNodes($node);
+    				} else {
+    					$result[] = trim(html_entity_decode($node->nodeValue));
+    				}
+    			}
+    		}
+    	}
+    	return $result;
+    }
+    
+    private function _hasChildNodes($parent_node)
+    {
+    	if ($parent_node->hasChildNodes()) {
+    		$nodes = $parent_node->childNodes;
+    		for ($i = 0; $i < $nodes->length; $i++) {
+    			if ($nodes->item($i)->nodeType == 1) {
+    				return true;
+    			}
+    		}
+    	}
+    	return false;
+    }
+    
+    private function _convertArrayToXML($array)
+    {
+    	$result = "";
+    	if (!empty($array)) {
+    		foreach ($array as $key=>$value) {
+    			$result.= is_numeric($key) ? "<item>" : "<$key>";
+    			if (is_array($value)) {
+    				$result.= $this->_convertArrayToXML($value);
+    			} else {
+    				$result.= htmlspecialchars($value);
+    			}
+    			$result.= is_numeric($key) ? "</item>" : "</$key>";
+    		}
+    	}
+    	return $result;
     }
 }
